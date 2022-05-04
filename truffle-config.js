4599d7d81fd34e9fcc5064942bf95a3a452db6ae
_safeMint <safemintyyx@gmail.com>
2022-05-03 22:41:34 -0500

    optimizer runs: 200

--- truffle-config.js
+++ truffle-config.js
@@ -103,7 +103,7 @@ module.exports = {
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
-         runs: 20
+         runs: 200
        },
       //  evmVersion: "byzantium"
       }
